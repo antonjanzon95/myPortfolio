4a42adc72e718f3bce386c3cbcdbedfaa882d212
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-21 15:54:38 +0100

    Added link to google maps

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -19,7 +19,12 @@ const Contact = () => {
         <div className="mb-8 flex flex-col items-start justify-center gap-4">
           <div className="flex items-center justify-center gap-2">
             <HiLocationMarker color="#f97316" size={28} />
-            Avesta, Sweden
+            <a
+              href="https://www.google.com/maps/place/Avesta/@60.140498,16.199,13z/data=!3m1!4b1!4m6!3m5!1s0x465de4b24ab6d509:0x95adb42dbab41716!8m2!3d60.14533!4d16.1738399!16s%2Fm%2F02p3hq7"
+              target="_blank"
+            >
+              Avesta, Sweden
+            </a>
           </div>
           <div className="flex items-center justify-center gap-2">
             <AiTwotonePhone color="#f97316" size={28} />
