1338e02d576d749d7c4186b28dd5cbf262516254
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-22 23:35:39 +0100

    Changed paragraph text and position of span

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,12 +19,6 @@ const About: React.FC<Props> = () => {
             and thrive in fast-paced environments. I am excited to share my
             portfolio with you and showcase my passion for Front End
             Development.
-            <span className="block text-center mt-7 font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-orange-600 to-gray-900">
-              <em>
-                I am actively seeking an internship opportunity from October 23
-                to December 29.
-              </em>
-            </span>
           </p>
         </div>
         <div className="laptop:flex items-center laptop:w-2/12">
@@ -34,23 +28,33 @@ const About: React.FC<Props> = () => {
             alt="Smiling man with black and white shirt"
           />
         </div>
+        <span className="block laptop:hidden text-center font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-orange-600 to-gray-900">
+          <em>
+            I am actively seeking an internship opportunity from October 23 to
+            December 29.
+          </em>
+        </span>
         <div className="laptop:w-5/12">
           <p className="mb-2">
             I understand the importance of hands-on experience in the field and
             am eager to apply the skills I have learned in a real-world setting.
-            I am open to various work arrangements, including in-house, hybrid,
-            or remote work, and am flexible to accommodate your needs. I am
-            confident that I would be able to make a meaningful contribution to
-            your organization and help drive your projects forward.
+            I am confident that I would be able to make a meaningful
+            contribution to your organization and help drive your projects
+            forward.
           </p>
-          <p className="text-justify">
+          <p>
             If you are interested in learning more about my qualifications and
             experience, or if you have an internship opportunity that you think
             would be a good fit, I would be more than happy to hear from you.
-            Please don't hesitate to reach out to me.
           </p>
         </div>
       </div>
+      <span className="hidden laptop:block text-center mt-7 font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-orange-600 to-gray-900">
+        <em>
+          I am actively seeking an internship opportunity from October 23 to
+          December 29.
+        </em>
+      </span>
     </div>
   );
 };
