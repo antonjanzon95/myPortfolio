3955bc8111399678b4ba68dcb2980eb8ec19c3cd
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-06-05 12:45:31 +0200

    added project & update email

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,49 +1,49 @@
-import { HiLocationMarker } from "react-icons/hi";
-import { AiTwotonePhone } from "react-icons/ai";
-import { FiMail } from "react-icons/fi";
-import SideBar from "./sidebar";
+import { HiLocationMarker } from 'react-icons/hi';
+import { AiTwotonePhone } from 'react-icons/ai';
+import { FiMail } from 'react-icons/fi';
+import SideBar from './sidebar';
 
 const Contact = () => {
   return (
-    <div className="container mx-auto relative">
-      <div className="flex flex-col justify-center items-center gap-4 pt-10 pb-4">
+    <div className='container mx-auto relative'>
+      <div className='flex flex-col justify-center items-center gap-4 pt-10 pb-4'>
         <a
-          href="mailto:antonjanzon123@gmail.com"
-          className="w-[188px] py-3 text-center border-solid border-2 border-gray-900 font-bold text-xl text-gray-100 bg-gradient-to-r from-orange-600 to-orange-700 hover:from-orange-900 hover:to-orange-800"
+          href='mailto:antonjanzon123@gmail.com'
+          className='w-[188px] py-3 text-center border-solid border-2 border-gray-900 font-bold text-xl text-gray-100 bg-gradient-to-r from-orange-600 to-orange-700 hover:from-orange-900 hover:to-orange-800'
         >
           Reach out to me!
         </a>
         <a
-          href="/Resume-Anton-Janzon.pdf"
-          className="w-[188px] py-3 text-center border-solid border-2 border-gray-900 font-bold text-xl text-gray-100 bg-gradient-to-r from-orange-600 to-orange-700 hover:from-orange-900 hover:to-orange-800"
-          target="_blank"
+          href='/Resume-Anton-Janzon.pdf'
+          className='w-[188px] py-3 text-center border-solid border-2 border-gray-900 font-bold text-xl text-gray-100 bg-gradient-to-r from-orange-600 to-orange-700 hover:from-orange-900 hover:to-orange-800'
+          target='_blank'
         >
           View resume
         </a>
         <SideBar size={32} />
       </div>
-      <address className="flex justify-center items-center">
-        <div className="mb-8 flex flex-col items-start justify-center gap-4">
-          <div className="flex items-center justify-center gap-2">
-            <HiLocationMarker color="#f97316" size={28} />
+      <address className='flex justify-center items-center'>
+        <div className='mb-8 flex flex-col items-start justify-center gap-4'>
+          <div className='flex items-center justify-center gap-2'>
+            <HiLocationMarker color='#f97316' size={28} />
             <a
-              href="https://www.google.com/maps/place/Avesta/@60.140498,16.199,13z/data=!3m1!4b1!4m6!3m5!1s0x465de4b24ab6d509:0x95adb42dbab41716!8m2!3d60.14533!4d16.1738399!16s%2Fm%2F02p3hq7"
-              target="_blank"
+              href='https://www.google.com/maps/place/Avesta/@60.140498,16.199,13z/data=!3m1!4b1!4m6!3m5!1s0x465de4b24ab6d509:0x95adb42dbab41716!8m2!3d60.14533!4d16.1738399!16s%2Fm%2F02p3hq7'
+              target='_blank'
             >
               Avesta, Sweden
             </a>
           </div>
-          <div className="flex items-center justify-center gap-2">
-            <AiTwotonePhone color="#f97316" size={28} />
+          <div className='flex items-center justify-center gap-2'>
+            <AiTwotonePhone color='#f97316' size={28} />
             (+46) 76 102 85 58
           </div>
-          <div className="flex items-center justify-center gap-2">
-            <FiMail color="#f97316" size={28} />
-            anton.janzon@medieinstitutet.se
+          <div className='flex items-center justify-center gap-2'>
+            <FiMail color='#f97316' size={28} />
+            antonjanzon123@gmail.com
           </div>
         </div>
       </address>
-      <p className="text-center pb-1 text-sm">
+      <p className='text-center pb-1 text-sm'>
         <em>Made with React, TypeScript & Tailwind.</em>
       </p>
     </div>
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,70 +4,109 @@ import {
   SiMongodb,
   SiExpress,
   SiSocketdotio,
-} from "react-icons/si";
-import { FaCss3, FaSass, FaNodeJs } from "react-icons/fa";
-import TechnologyIcon from "./techIcon";
+  SiTailwindcss,
+  SiTypescript,
+} from 'react-icons/si';
+import { FaCss3, FaSass, FaNodeJs, FaVuejs } from 'react-icons/fa';
+import TechnologyIcon from './techIcon';
 
 const Projects = () => {
   return (
-    <div className="container p-6 mx-auto relative max-w-[1024px]">
-      <div className="flex justify-center">
-        <h2 className="my-4 myHeadingsSlate text-orange-500">Projects</h2>
+    <div className='container p-6 mx-auto relative max-w-[1024px]'>
+      <div className='flex justify-center'>
+        <h2 className='my-4 myHeadingsSlate text-orange-500'>Projects</h2>
       </div>
-      <div className="my-6 flex flex-col items-center justify-center gap-10">
-        <div className="flex flex-col laptop:flex-row laptop:justify-center gap-10">
+      <div className='my-6 flex flex-col items-center justify-center gap-10'>
+        <div className='flex flex-col laptop:flex-row laptop:justify-center gap-10'>
           <a
-            href="https://glistening-tapioca-e79f13.netlify.app/"
-            target="_blank"
-            className="block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300"
+            href='https://glistening-tapioca-e79f13.netlify.app/'
+            target='_blank'
+            className='block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300'
           >
             <img
-              src="/gridmastercanvas.png"
-              alt="Gridpaint website"
-              className="h-[200px]"
+              src='public/tictactoe.png'
+              alt='TicTacToe image'
+              className='h-[200px]'
               width={384}
               height={200}
             />
-            <div className="p-6">
-              <h3 className="mb-2 text-xl font-bold tracking-tight text-slate-900">
-                Gridmaster Canvas
+            <div className='p-6 flex flex-col justify-between h-1/2'>
+              <h3 className='mb-2 text-xl font-bold tracking-tight text-slate-900'>
+                TicTacToe
               </h3>
-              <p className="font-normal text-slate-700">
-                Socket.io multiplayer grid painting app with chatroom.
+              <p className='font-normal text-slate-700'>
+                TicTacToe 1 on 1 with scores.
               </p>
-              <div className="flex gap-1 items-center justify-end my-0 mt-6">
+              <div className='flex gap-1 items-center justify-end my-0 mt-6'>
                 <TechnologyIcon
-                  icon={<SiHtml5 size={28} color="#f05c2a" />}
-                  iconType="bg-transparent-icon"
+                  icon={<FaVuejs size={28} color='#3fb984' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<FaSass size={28} color="#c76494" />}
-                  iconType="bg-transparent-icon"
+                  icon={<SiTypescript size={28} color='#2f74c0' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<SiJavascript size={28} color="yellow" />}
-                  iconType="javascript-icon"
-                />
-                <TechnologyIcon
-                  icon={<FaNodeJs size={28} color="#659a60" />}
-                  iconType="bg-transparent-icon"
-                />
-                <TechnologyIcon
-                  icon={<SiMongodb size={28} color="#4da53f" />}
-                  iconType="bg-transparent-icon"
-                />
-                <TechnologyIcon
-                  icon={<SiExpress size={28} color="#000000" />}
-                  iconType="bg-transparent-icon"
-                />
-                <TechnologyIcon
-                  icon={<SiSocketdotio size={28} color="#000000" />}
-                  iconType="bg-transparent-icon"
+                  icon={<SiTailwindcss size={28} color='#07b0ce' />}
+                  iconType='bg-transparent-icon'
                 />
               </div>
             </div>
           </a>
           <a
+            href='https://glistening-tapioca-e79f13.netlify.app/'
+            target='_blank'
+            className='block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300'
+          >
+            <img
+              src='/gridmastercanvas.png'
+              alt='Gridpaint website'
+              className='h-[200px]'
+              width={384}
+              height={200}
+            />
+            <div className='p-6 flex flex-col justify-between h-1/2'>
+              <h3 className='mb-2 text-xl font-bold tracking-tight text-slate-900'>
+                Gridmaster Canvas
+              </h3>
+              <p className='font-normal text-slate-700'>
+                Socket.io multiplayer grid painting app with chatroom.
+              </p>
+              <div className='flex gap-1 items-center justify-end my-0 mt-6'>
+                <TechnologyIcon
+                  icon={<SiHtml5 size={28} color='#f05c2a' />}
+                  iconType='bg-transparent-icon'
+                />
+                <TechnologyIcon
+                  icon={<FaSass size={28} color='#c76494' />}
+                  iconType='bg-transparent-icon'
+                />
+                <TechnologyIcon
+                  icon={<SiJavascript size={28} color='yellow' />}
+                  iconType='javascript-icon'
+                />
+                <TechnologyIcon
+                  icon={<FaNodeJs size={28} color='#659a60' />}
+                  iconType='bg-transparent-icon'
+                />
+                <TechnologyIcon
+                  icon={<SiMongodb size={28} color='#4da53f' />}
+                  iconType='bg-transparent-icon'
+                />
+                <TechnologyIcon
+                  icon={<SiExpress size={28} color='#000000' />}
+                  iconType='bg-transparent-icon'
+                />
+                <TechnologyIcon
+                  icon={<SiSocketdotio size={28} color='#000000' />}
+                  iconType='bg-transparent-icon'
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+
+        {/* <a
             href="https://antonjanzon95.github.io/Donut-Webshop/"
             target="_blank"
             className="block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300"
@@ -79,7 +118,7 @@ const Projects = () => {
               width={384}
               height={200}
             />
-            <div className="p-6">
+            <div className="p-6 flex flex-col justify-between h-1/2">
               <h3 className="mb-2 text-xl font-bold tracking-tight text-slate-700">
                 Donut Webshop
               </h3>
@@ -105,85 +144,84 @@ const Projects = () => {
                 />
               </div>
             </div>
-          </a>
-        </div>
+          </a> */}
 
-        <div className="flex flex-col laptop:flex-row gap-10">
+        <div className='flex flex-col laptop:flex-row gap-10'>
           <a
-            href="https://incomparable-seahorse-0f5220.netlify.app/"
-            target="_blank"
-            className="block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300"
+            href='https://incomparable-seahorse-0f5220.netlify.app/'
+            target='_blank'
+            className='block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300'
           >
             <img
-              src="/quiz-smaller.webp"
-              alt="Quiz web application"
-              className="h-[200px]"
+              src='/quiz-smaller.webp'
+              alt='Quiz web application'
+              className='h-[200px]'
               width={384}
               height={200}
             />
-            <div className="p-6">
-              <h3 className="mb-2 text-xl font-bold tracking-tight text-slate-700">
+            <div className='p-6 flex flex-col justify-between h-1/2'>
+              <h3 className='mb-2 text-xl font-bold tracking-tight text-slate-700'>
                 Quiz Game
               </h3>
-              <p className="font-normal text-slate-700">
+              <p className='font-normal text-slate-700'>
                 A quiz with different categories and difficulties. Questions are
                 fetched from an API.
               </p>
-              <div className="flex gap-1 items-center justify-end my-0 mt-6">
+              <div className='flex gap-1 items-center justify-end my-0 mt-6'>
                 <TechnologyIcon
-                  icon={<SiHtml5 size={28} color="#f05c2a" />}
-                  iconType="bg-transparent-icon"
+                  icon={<SiHtml5 size={28} color='#f05c2a' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<FaCss3 size={28} color="#1774bb" />}
-                  iconType="bg-transparent-icon"
+                  icon={<FaCss3 size={28} color='#1774bb' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<FaSass size={28} color="#c76494" />}
-                  iconType="bg-transparent-icon"
+                  icon={<FaSass size={28} color='#c76494' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<SiJavascript size={28} color="yellow" />}
-                  iconType="javascript-icon"
+                  icon={<SiJavascript size={28} color='yellow' />}
+                  iconType='javascript-icon'
                 />
               </div>
             </div>
           </a>
           <a
-            href="https://resonant-tulumba-7148cf.netlify.app/"
-            target="_blank"
-            className="block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300"
+            href='https://resonant-tulumba-7148cf.netlify.app/'
+            target='_blank'
+            className='block max-w-[340px] laptop:max-w-sm bg-slate-100 shadow hover:bg-slate-200 hover:scale-105 transition-all duration-300'
           >
             <img
-              className="h-[200px]"
-              src="/nothingPhone-smaller.webp"
-              alt="Phone advert web app"
+              className='h-[200px]'
+              src='/nothingPhone-smaller.webp'
+              alt='Phone advert web app'
               width={384}
               height={200}
             />
-            <div className="p-6">
-              <h3 className="mb-2 text-xl font-bold tracking-tight text-slate-900">
+            <div className='p-6 flex flex-col justify-between h-1/2'>
+              <h3 className='mb-2 text-xl font-bold tracking-tight text-slate-900'>
                 Nothing Phone
               </h3>
-              <p className="font-normal text-slate-700">
+              <p className='font-normal text-slate-700'>
                 Developed according to given design and graphical profile.
               </p>
-              <div className="flex gap-1 items-center justify-end my-0 mt-6">
+              <div className='flex gap-1 items-center justify-end my-0 mt-6'>
                 <TechnologyIcon
-                  icon={<SiHtml5 size={28} color="#f05c2a" />}
-                  iconType="bg-transparent-icon"
+                  icon={<SiHtml5 size={28} color='#f05c2a' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<FaCss3 size={28} color="#1774bb" />}
-                  iconType="bg-transparent-icon"
+                  icon={<FaCss3 size={28} color='#1774bb' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<FaSass size={28} color="#c76494" />}
-                  iconType="bg-transparent-icon"
+                  icon={<FaSass size={28} color='#c76494' />}
+                  iconType='bg-transparent-icon'
                 />
                 <TechnologyIcon
-                  icon={<SiJavascript size={28} color="yellow" />}
-                  iconType="javascript-icon"
+                  icon={<SiJavascript size={28} color='yellow' />}
+                  iconType='javascript-icon'
                 />
               </div>
             </div>
